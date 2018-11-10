77ab93ee65cefa8d7d8181268d445327e7dc8f2e
Karol Adamiak <karol.kosciany@gmail.com>
2018-11-10 13:46:46 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,9 +1,11 @@
 import pyknow
 import easygui
 
-tytuł_okna = "Baza wiedzy materiałów"
+tytuł_okna = "Baza wiedzy materiałów" #tytuł okna interfejsu graficznego
 
 def zwroc_odpowiedz(pytanie,możliwe_odpowiedzi):
+    #funkcja, która otwira okienko z pytaniem podanym w argumencie "pytanie" i przyciskami z odpowiedziami zawartymi
+    #w liście możliwe odpowiedzi. Buttonbox zwraca wybraną odpowiedź. Funkcja zwraca wybraną odpowiedź.
     zwrot=easygui.buttonbox(pytanie, title=tytuł_okna, choices=możliwe_odpowiedzi)
     print(zwrot)
     return zwrot
@@ -11,17 +13,19 @@ def zwroc_odpowiedz(pytanie,możliwe_odpowiedzi):
 class Baza_wiedzy_materialow(pyknow.KnowledgeEngine):
     @pyknow.DefFacts()
     def zdefiniuj_fakty(self):
-        yield pyknow.Fact(wytrzymalosc=zwroc_odpowiedz("Wymagana wytrzymałość materiału", ["Podwyższona", "Normalna"]))
+        #użyto dekorator pyknow.DefFacts, funkcja odpalana po wywołaniu metory .run() obiektu
+        yield pyknow.Fact(wytrzymalosc=zwroc_odpowiedz("Wymagana wytrzymałość materiału", ["Podwyższona", "Normalna"])) #generowanie kolejnych faktów)
         yield pyknow.Fact(koszt=zwroc_odpowiedz("Koszt materialu", ["Niski", "Nie gra roli"]))
         yield pyknow.Fact(plastycznosc=zwroc_odpowiedz("Dobra plastyczność materiału",["Wymagana","Niewymagana"]))
         yield pyknow.Fact(odpornosc_na_korozje=zwroc_odpowiedz("Odporność na korozję",["Wymagana","Niewymagana"]))
 
-    @pyknow.Rule(pyknow.AND(pyknow.Fact(wytrzymalosc="Normalna"),
+
+    @pyknow.Rule(pyknow.AND(pyknow.Fact(wytrzymalosc="Normalna"), #utworzenie reguły i określenie faktów, które warunkują jej wywołanie
                             pyknow.Fact(koszt="Nie gra roli"),
                             pyknow.Fact(plastycznosc="Niewymagana"),
                             pyknow.Fact(odpornosc_na_korozje="Niewymagana")),
-                 salience=1)
-    def material_10(self):
+                 salience=1) #salience - priorytet zasad, im wyższa liczba tym wyższy priorytet
+    def material_10(self): #funkcja która jest wykonywana, kiedy powyższa zasada jest spełniona
         easygui.msgbox('Wybrano miedź', title=tytuł_okna)
 
     @pyknow.Rule(pyknow.AND(pyknow.Fact(wytrzymalosc="Normalna"),

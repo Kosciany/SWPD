f021d2910d50b89afd69bfc6bb6256c05c1f217a
Karol Adamiak <karol.kosciany@gmail.com>
2018-11-12 17:03:24 +0100

    Update and rename ekspercki.py to and_gate.py

--- and_gate.py
+++ and_gate.py
@@ -9,6 +9,7 @@ class AND_Gate(pyknow.KnowledgeEngine):
     @pyknow.DefFacts()
     def needed_data(self):
         yield pyknow.Fact(First_value = input("Podaj pierwszą wartość"))
+        yield pyknow.Fact(Second_value = input("Podaj drugą wartość"))
     @pyknow.Rule(pyknow.AND(First_value("True"), Second_value("True")))
     def gate_open(self):
         print("GATE OPEN")
@@ -18,5 +19,4 @@ class AND_Gate(pyknow.KnowledgeEngine):
 
 engine = AND_Gate()
 engine.reset()
-engine.declare(Second_value("False"))
 engine.run()
